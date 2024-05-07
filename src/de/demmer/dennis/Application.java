4713e5e056935038a6b5f278ff55a460cd9f11f5
Dennis <dennis.demmer1@gmail.com>
2024-05-07 10:37:57 +0200

    initial commit

---
+++ src/de/demmer/dennis/Application.java
@@ -0,0 +1,24 @@
+package de.demmer.dennis;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		
+		
+		int[] iArr = new int[20];
+		testMethod();
+		iArr[5] = 250;
+		
+		Item i1 = new Item(123, 9.99, "Mauspad");
+		
+		System.out.println(iArr[2]);
+		
+	}
+	
+	
+	
+	static void testMethod() {
+		System.out.println("test");
+	}
+
+}
